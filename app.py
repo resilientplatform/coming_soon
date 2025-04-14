f832324e14479ed4ce1c4541a821be12b43eeaa4
resilientplatform <ppenasb@gmail.com>
2025-04-14 13:10:56 -0700

    Updated app.py to fix unfound site.manifest problem

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
-@app.route("/")
-def coming_soon():
-    return render_template("index.html")
+@app.route('/site.webmanifest')
+def manifest():
+    return send_from_directory('.', 'site.webmanifest')
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+@app.route('/')
+def coming_soon():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
